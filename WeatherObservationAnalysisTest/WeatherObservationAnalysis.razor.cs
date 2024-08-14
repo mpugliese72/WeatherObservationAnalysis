20cee3918cf706382055e182626692dd6af458f4
Matthew Pugliese <matelax41@gmail.com>
2024-08-13 19:21:13 -0600

    small touch up

--- WeatherObservationAnalysisTest/WeatherObservationAnalysis.razor.cs
+++ WeatherObservationAnalysisTest/WeatherObservationAnalysis.razor.cs
@@ -7,6 +7,23 @@ namespace WeatherObservationAnalysisTest
     public class WeatherObservationAnalysisTest
     {
         [Fact]
+        public void GetClosestWeatherStationIncorrectValue()
+        {
+            // Arrange
+            WeatherObservation weatherObservation = new WeatherObservation();
+
+            // Act
+            var result = weatherObservation.GetClosestWeatherStation("987654");
+            
+            // Assert
+            if (result == null)
+            {
+                var message = "No results for inputted weather station";
+                Assert.Fail(string.Format(message, result));
+            }
+        }
+
+        [Fact]
         public void GetClosestWeatherStationNull()
         {
             // Arrange
